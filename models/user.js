02c3cd0b1487a0be27029c105eec2e994f3db1a1
Nilesh-Aditya <nileshaditya2000@gmail.com>
2021-10-23 21:20:15 +0530

    schema user updated

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {CourseSchema} = require('./course.js')
+const {CourseSchema} = require('./course.js');
+const CourseSegmentSchema = require('./coursesegment.schema.js');
 
 const UserSchema = new Schema({
     name: String,
     bio : String,
+    recents: [CourseSchema._id],
     friends : [UserSchema._id],
     courses : [CourseSchema._id],
     usermadecourses = [CourseSchema._id],
