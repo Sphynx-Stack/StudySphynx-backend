4113be7ba38d43d276a4fa24d87a566b41f98f4f
Nilesh-Aditya <nileshaditya2000@gmail.com>
2021-10-24 14:11:51 +0530

    routes updations

--- public/js/logout.js
+++ public/js/logout.js
@@ -6,21 +6,3 @@ document.querySelector('.LOGOUT').addEventListener('click', async (e) => {
     let data = await res.json();
     window.location.href = data.redirect;
 })
-
-async function connect(dest) {
-  const res = await fetch(`/api/${dest}`, {
-      method: 'GET'
-  });
-  const data = await res.json();
-  return data;
-}
-
-function connection(dest) {
-  connect(dest).then(response => {
-    return response;
-  }).catch(err => console.log(err));
-}
-
-document.addEventListener('DOMContentLoaded', (e) => {
-  let data = connection('user')
-})
