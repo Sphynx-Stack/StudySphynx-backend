b48bd2133fe052195df45948225d6d4f4ee2e8c3
Prashubh Atri <prashubh27@gmail.com>
2021-10-24 19:50:59 +0530

    Dost dost nah raha

--- routes/api/user.js
+++ routes/api/user.js
@@ -35,12 +35,15 @@ route.get('/friends/:friend_id', async (req, res) => {
     }
 })
 
-route.put('/friends/:friend_id', async (req, res) => {
-    let friend_id = req.params.friend_id;
+route.put('/friends/:friend_id', userID,  async (req, res) => {
     try {
-        let friend = await User.findOne({ user_id :friend_id });
-        
-        // unshift friends prashubh
+        let friend = await User.findOne({ user_id : req.params.friend_id });
+        let user = await User.findOne({user_id: req.user_id});
+        user.friends.unshift(req.params.friend_id);
+        friend.friends.unshift(req.user_id);
+        await user.save();
+        await friend.save();
+        return res.json({msg: "Added as a friend."});
     } catch (error) {
         console.error(error);
         res.status(400).json({error});
