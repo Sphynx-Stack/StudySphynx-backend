55995ffde534a2e7d211bae52e5553b2eae95481
Nilesh-Aditya <nileshaditya2000@gmail.com>
2021-10-24 14:43:05 +0530

    routes updations

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,6 @@ route.use('/login', require('./login'));
 route.use('/course', require('./course'));
 // route.use('/temp', require('./testing'));
 route.use('/logout', require('./logout'));
-// route.use('/user', require('./user'));
+route.use('/user', require('./user'));
 
 module.exports = route;
