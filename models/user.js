4ceda4ccefa042f5708f987bfc2db12f2fb1b964
unknownblueguy6 <unknownblueguy6@gmail.com>
2021-10-24 14:49:18 +0530

    fix: require NotificationSchema and EventSchema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const {CourseSchema} = require('./course.js');
-const CourseSegmentSchema = require('./coursesegment.schema.js');
+const NotificationSchema = require('./notification.schema.js');
+const EventSchema = require('./event.schema');
 
 const UserSchema = new Schema({
     name: String,
